25c7460ccaec8f10e03bb3f946266cec89177b0c
salastro <salah2112004@gmail.com>
2024-06-29 17:14:29 +0300

    104

---
+++ 104.cpp
@@ -0,0 +1,62 @@
+#include <climits>
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+  int max_depth = INT_MIN;
+
+public:
+  int maxDepth(TreeNode *root) {
+    if (!root)
+      return 0;
+
+    dfs(root, 0);
+
+    return max_depth;
+  }
+
+  void dfs(TreeNode *node, int depth) {
+    if (!node)
+      return;
+
+    if (!node->left && !node->right) {
+      max_depth = max(max_depth, depth + 1);
+    }
+
+    dfs(node->left, depth + 1);
+    dfs(node->right, depth + 1);
+  }
+};
+
+int main() {
+  // // root = [3,9,20,null,null,15,7]
+  TreeNode *root1 = new TreeNode(3);
+  root1->left = new TreeNode(9);
+  root1->right = new TreeNode(20);
+  root1->right->left = new TreeNode(15);
+  root1->right->right = new TreeNode(7);
+
+  // root = [2,null,3,null,4,null,5,null,6]
+  TreeNode *root2 = new TreeNode(2);
+  root2->right = new TreeNode(3);
+  root2->right->right = new TreeNode(4);
+  root2->right->right->right = new TreeNode(5);
+  root2->right->right->right->right = new TreeNode(6);
+
+  Solution sol1;
+  cout << sol1.maxDepth(root1) << endl;
+  Solution sol2;
+  cout << sol2.maxDepth(root2) << endl;
+
+  return 0;
+}
